35957aeb6bdbb9ee0194201f971111319bb41933
Kebirs <dklecha99@gmail.com>
2021-02-04 21:48:48 +0100

    first interaction

---
+++ download_mod.py
@@ -0,0 +1,6 @@
+
+from selenium import webdriver
+
+browser = webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe')
+browser.get('https://aslain.legionriders.club/Aslains_WoT_Modpack_Installer_v.1.11.1.2_01.exe.torrent')
+
