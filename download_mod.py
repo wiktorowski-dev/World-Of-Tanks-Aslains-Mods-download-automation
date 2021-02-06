9c25a67e72e5d8bafa10199aae2eaa3140279f02
Kebirs <dklecha99@gmail.com>
2021-02-06 12:24:48 +0100

    almost done, no instalation run yet

--- download_mod.py
+++ download_mod.py
@@ -1,17 +1,15 @@
-from selenium import webdriver
+import os
 import subprocess
 import time
-import os
+import re
+
+from selenium import webdriver
 
 
 class ModDownload():
 
     def __init__(self):
         self.browser = webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe')
-        print('Mod do pobierania najnowszych modow Aslaina do World Of Tanks')
-        time.sleep(1.0)
-        print('AlainModsAutoDownload ver. 1.0')
-        time.sleep(1.0)
 
     def get_page(self):
         self.browser.get('https://aslain.com/index.php?/topic/13-download-%E2%98%85-world-of-tanks-%E2%98%85-modpack/')
@@ -23,38 +21,57 @@ class ModDownload():
         self.accept.click()
         time.sleep(1)
 
-    def get_mod(self):
-        self.acceptbutton = self.browser.find_elements_by_xpath(
+    def mod_download(self):
+        self.direct = self.browser.find_elements_by_xpath(
             '//*[@id="comment-13_wrap"]/div[2]/div[1]/p[8]/span[1]/a')[0]
         time.sleep(1)
-        self.acceptbutton.click()
+        self.direct.click()
 
-    # def show_downloaded_files(self):
-    #     self.browser.get('chrome://downloads/')
-    #     time.sleep(1)
-    #
-    # def download_installator(self):
-    #     self.installator = self.browser.find_elements_by_xpath('//*[@id="file-link"]')
-    #     time.sleep(1)
-    #     self.installator.click()
+    def get_mod_version_number(self):
+        self.version = self.browser.find_elements_by_xpath(
+            '//*[@id="comment-13_wrap"]/div[2]/div[1]/p[7]/span[1]/span/strong')
 
-# TODO podmienic nazwe wersji installera pobierajac scraperem aktualna wersje ze strony, i dokonczyc przeklikanie instalacji
+        ver_data = []
 
+        for value in self.version:
+            ver_data.append(value.text)
+            # print(value.text)
+
+        text = ver_data[0]
+
+        m = re.search('ModPack v(.+?) ', text)
+        if m:
+            version_num = m.group(1)
+        # print(version)
+        return version_num
+
+    def get_next_number(self):
+        self.number = self.browser.find_elements_by_xpath(
+            '//*[@id="comment-13_wrap"]/div[2]/div[1]/p[7]/span[2]/span/strong')
+
+        num_data = []
+
+        for v in self.number:
+            num_data.append(v.text)
+            v.text.replace('#', '_')
+            # print(v.text)
+
+        number = num_data[0].replace('#', '_')
+        # print(number)
+
+        return number
 
     def get_installer_from_dir(self):
         directory = r"C:\Users\dklec\Downloads"
-        subprocess.Popen(os.path.join(directory, "Aslains_WoT_Modpack_Installer_v.1.11.1.2_02.exe"))
-
-
-
-
-
+        subprocess.Popen(os.path.join(directory, "Aslains_WoT_Modpack_Installer_v."
+                                      + self.get_mod_version_number()
+                                      + self.get_next_number() + ".exe"))
 
 
 mods = ModDownload()
 mods.get_page()
 mods.accept_cookies()
-mods.get_mod()
-# mods.show_downloaded_files()
-# mods.download_installator()
+mods.mod_download()
+mods.get_mod_version_number()
+mods.get_next_number()
 mods.get_installer_from_dir()
